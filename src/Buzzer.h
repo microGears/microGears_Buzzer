31241b0297629df6017d99e30deb77cef71a5c91
CuriosMax <kirichenko.maxim@gmail.com>
2023-04-12 19:35:57 +0300

    Update

--- src/Buzzer.h
+++ src/Buzzer.h
@@ -7,29 +7,25 @@
 #include "WProgram.h"
 #endif
 
-#define BD_SHORT 75
-#define BD_LONG 750                 // длиный сигнал
-#define BD_LONG_ALT BD_LONG * -1    // пауза
-#define BD_SCENARIO_LENGTH 8        // размер сценария
+#define BD_SHORT 75             // короткий сигнад
+#define BD_LONG 750             // длиный сигнал
+#define BD_SCENARIO_LENGTH 8    // макс. размер сценария
 #define _fill(a, n) a[0] = n, memcpy(((char*)a) + sizeof(a[0]), a, sizeof(a) - sizeof(a[0]));
 
-typedef void (*beep_callback_t)(uint8_t _val);
+typedef void (*beep_callback_t)(uint8_t val);
 typedef int16_t beep_scenario_t[BD_SCENARIO_LENGTH];    // сценарий
-
-#define BS_short4x \
-    { BD_SHORT, BD_SHORT, BD_SHORT, BD_SHORT }
 
 class microGears_Buzzer {
     public:
     microGears_Buzzer(){};
 
-    void init(uint8_t _pin) {
-        pin = _pin;
-        pinMode(pin, OUTPUT);
+    void init(uint8_t pin) {
+        __pin = pin;
+        pinMode(__pin, OUTPUT);
     }
 
-    void setCallbackHandler(beep_callback_t _callback) {
-        callback = _callback;
+    void setCallbackHandler(beep_callback_t callback) {
+        __callback = callback;
     }
 
     void beep(beep_scenario_t scenario, uint32_t repeat = 1) {
@@ -37,63 +33,81 @@ class microGears_Buzzer {
         int16_t durability = 0;
 
         noBeep();
-        _fill(_scenario, 0);
+        _fill(__scenario, 0);
         while ((durability = scenario[size]) && size <= BD_SCENARIO_LENGTH) {
-            _scenario[size] = constrain(durability, BD_LONG * -1, BD_LONG);
+            __scenario[size] = constrain(durability, BD_LONG * -1, BD_LONG);
             size++;
         }
 
-        _scenario_key = 0;
-        _repeat       = ((_scenario_length = size) * 2) * repeat;
+        __scenario_key = 0;
+        __repeat       = ((__scenario_length = size) * 2) * repeat;
+    }
+
+    void beep(uint16_t duration, uint32_t repeat = 1) {
+        noBeep();
+        duration = constrain(duration, BD_SHORT, BD_LONG);
+        _fill(__scenario, duration);
+        __scenario_key = 0;
+        __repeat       = (__scenario_length = repeat) * 2;
+    }
+
+    void circulate(beep_scenario_t scenario) {
+        beep(scenario, 1);
+        __loop = true;
+    }
+
+    void circulate(uint16_t duration) {
+        beep(duration, 1);
+        __loop = true;
     }
 
     void noBeep() {
-        _repeat = 0;
-        _loop   = false;
+        __repeat = 0;
+        __loop   = false;
         setState(LOW);
     }
-    
+
     void tick() {
         static unsigned long last_request = 0;
         unsigned long        now          = millis();
 
         int32_t duration = getDuration();
         bool    rising   = false;
-        if ((now - last_request) > abs(duration) && _repeat > 0) {
+        if ((now - last_request) > abs(duration) && __repeat > 0) {
 
             last_request = now;
-            uint8_t val  = (rising = --_repeat % 2) && duration > 0 ? HIGH : LOW;
+            uint8_t val  = (rising = --__repeat % 2) && duration > 0 ? HIGH : LOW;
             setState(val);
 
             if (!rising) {
-                _scenario_key = (_scenario_key + 1) % _scenario_length;
+                __scenario_key = (__scenario_key + 1) % __scenario_length;
             }
 
-            if (_repeat == 0 && _loop) {
-                _repeat = 2;
+            if (__repeat == 0 && __loop) {
+                __repeat = 2;
             }
         }
     }
 
     private:
-    uint8_t         pin;
-    beep_callback_t callback;
-    uint32_t        _repeat          = 0;
-    bool            _loop            = false;
-    beep_scenario_t _scenario        = {0};
-    byte            _scenario_length = 0;
-    byte            _scenario_key    = 0;
+    uint8_t         __pin;
+    beep_callback_t __callback;
+    uint32_t        __repeat          = 0;
+    bool            __loop            = false;
+    beep_scenario_t __scenario        = {0};
+    byte            __scenario_length = 0;
+    byte            __scenario_key    = 0;
 
-    void setState(uint8_t _val) {
-        if (callback != nullptr) {
-            callback(_val);
+    void setState(uint8_t val) {
+        if (__callback != nullptr) {
+            __callback(val);
             return;
         }
 
         /** else self apply */
-        digitalWrite(pin, _val);
+        digitalWrite(__pin, val);
     }
 
-    int32_t getDuration() { return _scenario[_scenario_key]; }
+    int32_t getDuration() { return __scenario[__scenario_key]; }
 };
 #endif
